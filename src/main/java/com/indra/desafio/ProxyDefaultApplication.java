55c744f06cd0e0535ee5f51516c3571893eadca7
root <root@localhost.localdomain>
2018-09-19 09:54:53 -0400

    Funcionando

---
+++ src/main/java/com/indra/desafio/ProxyDefaultApplication.java
@@ -0,0 +1,12 @@
+package com.indra.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyDefaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyDefaultApplication.class, args);
+	}
+}
---
+++ src/test/java/com/indra/desafio/ProxyDefaultApplicationTests.java
@@ -0,0 +1,16 @@
+package com.indra.desafio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProxyDefaultApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
